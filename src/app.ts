d0c45833c142ecf083c08b7be657458f68b8a2a6
Quoc Du <adward0511@gmail.com>
2019-04-02 09:41:31 +0700

    BAse node typescript

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import flash from "express-flash";
 import path from "path";
+import morgan from "morgan";
 import expressValidator from "express-validator";
 import { SESSION_SECRET } from "./util/secrets";
 
@@ -18,6 +19,7 @@ const app = express();
 app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "ejs");
+app.use(morgan("dev"));
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -35,7 +37,18 @@ app.use(
 app.get("/", (req, res, next) => {
     res.render("index");
 });
+app.post("/post", (req, res, next) => {
+    req.assert("email", "Email is not valid").isEmail();
 
+    const errors = req.validationErrors();
+    console.log(errors);
+    if (errors) {
+        return res.status(400).json({
+            error: errors
+        });
+    }
+    return res.json("index");
+});
 /**
  * Primary app routes.
  */
